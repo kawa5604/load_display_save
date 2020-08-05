fdc432f497aebf89926e58d075f120f412b34ad5
kawa5604 <49937226+kawa5604@users.noreply.github.com>
2020-08-05 10:57:09 -0700

    Update load_save_display.py
    Fixed shebang

--- load_save_display.py
+++ load_save_display.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import argparse
 import cv2
 
@@ -5,3 +7,21 @@ import cv2
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="Path to the image")
 args = vars(ap.parse_args())
+
+# Load the image from the argument and load it with cv2
+# The function cv2.imread will return a numpy array that represents the image
+
+image = cv2.imread(args["image"])
+
+# Printing the image properties
+print("height: {} pizels".format(image.shape[0]))
+print("width: {} pixels".format(image.shape[1]))
+print("channels: {}".format(image.shape[2]))
+
+# Displays the image
+cv2.imshow("image", image)
+# Waits for any key press to unpause execution
+cv2.waitKey(0)
+# Save the image as jpg
+cv2.imwrite("newimage.jpg", image)
+
